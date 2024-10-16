77270bf698979098f0b2e77d5fc901d4fd79d355
GuoJun <dosius.cgj@gmail.com>
2024-10-17 02:24:31 +0800

    Remove unnecessary comments

--- app(To send in).py
+++ app(To send in).py
@@ -1007,6 +1007,4 @@ def update_profile():
         return redirect(url_for('profile'))
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    #app.run(debug=True, host='0.0.0.0', port=5000)
     app.run(debug=True, host='23.23.130.142', port=5000)
